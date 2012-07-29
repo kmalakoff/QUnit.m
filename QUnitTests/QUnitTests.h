99a8a7f71de7dc9a7759c4e54a35e6fd032280d6
Kevin Malakoff <xmann.intl@gmail.com>
2012-07-29 18:00:26 +0900

    Initial commit (fresh project)

---
+++ QUnitTests/QUnitTests.h
@@ -0,0 +1,13 @@
+//
+//  QUnitTests.h
+//  QUnitTests
+//
+//  Created by Kevin Malakoff on 7/29/12.
+//  Copyright (c) 2012 Kevin Malakoff. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface QUnitTests : SenTestCase
+
+@end
